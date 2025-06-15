b6ca56836ac378ebb5dc848b6423aff3f5422152
smer44 <34803459+smer44@users.noreply.github.com>
2025-06-15 02:01:19 +0200

    Delete BattleLogic directory

--- BattleLogic/UnitSO.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "UnitSO", menuName = "Scriptable Objects/UnitSO")]
-public class UnitSO : ObjectWithHitPointsSO
-{
-    public int attack;
-    public int defence;
-}
